b947d214c0f9b90191c403ceca48645d6c7bf1c9
hyhweb <461342854@qq.com>
2019-10-29 16:15:32 +0800

    code

--- src/main/java/com/springboot/swagger/controller/UserController.java
+++ src/main/java/com/springboot/swagger/controller/UserController.java
@@ -1,12 +1,29 @@
 package com.springboot.swagger.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(tags = "用户管理相关接口")
+@RequestMapping("/user")
 public class UserController {
-    @PostMapping("/hello")
-    public String hello(){
-    return "hello";
+    @PostMapping("/add")
+    @ApiOperation("添加用户的接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", defaultValue = "李四"),
+            @ApiImplicitParam(name = "address", value = "用户地址", defaultValue = "深圳", required = true)
+    }
+    )
+    public String hello(String username, @RequestParam(required = true) String address){
+         return "hello";
+    }
+    @GetMapping("/getUserById")
+    @ApiOperation("根据id查询用户的接口")
+    @ApiImplicitParam(name = "id", value = "用户id", defaultValue = "99", required = true)
+    public String getUserById(@PathVariable Integer id) {
+        return "user";
     }
 }
